33767f234edbfe9927822b01f19366b86cd0515c
jmendoza <jmendoza@cgtscorp.com>
2019-08-31 22:47:11 -0400

    Software design pattern: Update Facade

---
+++ Structural-Patterns/facadepattern/src/main/java/com/jmendoza/springboot/facadepattern/FacadepatternApplication.java
@@ -0,0 +1,13 @@
+package com.jmendoza.springboot.facadepattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacadepatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacadepatternApplication.class, args);
+    }
+
+}
