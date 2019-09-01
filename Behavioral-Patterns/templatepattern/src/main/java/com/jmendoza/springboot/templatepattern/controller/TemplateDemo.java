42df8fcb49d1a125161109c1396091008665333d
jmendoza <jmendoza@cgtscorp.com>
2019-08-31 21:47:40 -0400

    Software design pattern: Update Template Method

--- Behavioral-Patterns/templatepattern/src/main/java/com/jmendoza/springboot/templatepattern/controller/TemplateDemo.java
+++ Behavioral-Patterns/templatepattern/src/main/java/com/jmendoza/springboot/templatepattern/controller/TemplateDemo.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Define the skeleton of an algorithm in an operation, deferring some steps to subclasses.
+ * Template method lets subclasses redefine certain steps of an algorithm without changing the algorithm's structure.
+ */
+
 @RestController
 @RequestMapping("/template")
 public class TemplateDemo {
